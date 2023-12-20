b1341794ffced90d66f79b8447222547907d8bf7
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-12-20 19:35:01 +0530

    Latest commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://main--radiant-quokka-c36440.netlify.app');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
